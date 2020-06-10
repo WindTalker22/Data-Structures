ba373e1533b74385aeed9a0f594c161db5ab33d7
Stephen Saciolo <stephensaciolo@Stephens-MBP.fios-router.home>
2020-06-10 15:46:47 -0400

    insert and contains pass

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,8 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,14 +19,41 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # Check if there is a node that already exists
+        if self is None:
+            # If no node exists we create a new node
+            return BSTNode(value)
+
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
+
     def contains(self, target):
-        pass
+
+        if self.value is target:
+            return True
+
+        if self.value > target:
+            if self.left is not None:
+                return self.left.contains(target)
+        elif self.value < target:
+            if self.right is not None:
+                return self.right.contains(target)
+        else:
+            return False
 
     # Return the maximum value found in the tree
+
     def get_max(self):
         pass
 
